da47692b1fdc1a3f042a35efab078e6c3fc6760b
binarycat <binarycat@envs.net>
2024-03-18 15:59:29 -0400

    nixpkgs-whereis: init at 1.0.0

---
+++ pkgs/by-name/nixpkgs-whereis/package.nix
@@ -0,0 +1,43 @@
+{ lib
+, stdenv
+, fetchFromGitea
+, testers
+, makeWrapper
+, coreutils
+, fish
+, nix
+}:
+stdenv.mkDerivation (finalAttrs: {
+  pname = "nixpkgs-whereis";
+  version = "1.0.0";
+
+  src = fetchFromGitea {
+    domain = "git.envs.net";
+    owner = "binarycat";
+    repo = "nixpkgs-whereis";
+    rev = finalAttrs.version;
+    hash = "sha256-cpkPRK1Vaq12PcEokMNVml7k5kO2NptZjebVOVhKXYE=";
+  };
+
+  nativeBuildInputs = [ makeWrapper fish nix coreutils ];
+
+  passthru.tests.version = testers.testVersion {
+    package = finalAttrs.finalPackage;
+    command = "nixpkgs-whereis --version";
+    inherit (finalAttrs) version;
+  };
+
+  makeFlags = [ "PREFIX=$(out)" ];
+
+  postFixup = ''
+    patchShebangs $out/bin/nixpkgs-whereis
+    wrapProgram $out/bin/nixpkgs-whereis \
+      --prefix PATH : ${lib.makeBinPath [ nix coreutils fish ]}
+  '';
+
+  meta = with lib; {
+    description = "A simple command to check where in nixpkgs an attribute is defined";
+    homepage = "https://git.envs.net/binarycat/nixpkgs-whereis";
+    maintainer = with maintainers; [ binarycat ];
+  };
+})
