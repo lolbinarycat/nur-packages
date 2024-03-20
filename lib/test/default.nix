21d7868f458773955f388ab30c7874cca1aba292
binarycat <binarycat@envs.net>
2024-03-20 01:34:57 -0400

    lib.testers.urlExists: add --max-time to curl

--- lib/test/default.nix
+++ lib/test/default.nix
@@ -29,7 +29,8 @@ in rec {
     } ''
       echo -e "checking if url exists:\t${u}"
       # disable ssl check because nix hides certs
-      curl --insecure --fail -ILXHEAD ${u}
+      # timeout to stop this from hanging forever
+      curl --max-time 300 --insecure --fail -ILXHEAD ${u}
       touch $out
   '';
 
