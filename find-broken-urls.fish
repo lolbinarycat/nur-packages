3928587fcdee5ff94ac5d551768328920fe140c1
binarycat <binarycat@envs.net>
2024-03-19 22:30:51 -0400

    update the silly little script

--- find-broken-urls.fish
+++ find-broken-urls.fish
@@ -1,12 +1,17 @@
 #!/usr/bin/env fish
 
-# given a certian prefix to check, check that packages with that prefix don't have any proken urls
+# must be run as a trusted user in order to disable sandbox (needed for network access)
 
-# this piecewise method is needed because otherwise nix uses too much memory
+set -l abc a b c d e f g h i j k l m n o p q r s t u v w x y z 1 2 3 4 5 6 7 8 9 _ -
 
-# use sudo because we need to be a privalaged user
-# -j 1 to make our log output linear
-sudo nix-build --keep-going -j 1 --show-trace --no-sandbox -E '(import ./. {}).lib.testers.checkMetaUrlsPkgPrefix "'$argv[1]'"' 2>/tmp/url-log-err >/tmp/url-log-out
+# carteasean product
+for prefix in $abc$abc
+	echo >/proc/self/fd/0 prefix: $prefix
+	# given a certian prefix to check, check that packages with that prefix don't have any proken urls
+	# this piecewise method is needed because otherwise nix uses too much memory
+	# -j 1 to make our log output linear
+nix-build --keep-going -j 1 --show-trace --no-sandbox -E '(import ./. {}).lib.testers.checkMetaUrlsPkgPrefix "'$prefix'"' 2>/tmp/url-log-err >/tmp/url-log-out
+end
 
 # for every derivation that failed to build, we find the previous
 # log entry that says a url is being fetched
